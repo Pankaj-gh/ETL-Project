ed009fd32f9023e7464e48d59ddfccb9eaec7cca
Pankaj <tahiliani.pankaj19@gmail.com>
2020-10-24 11:18:39 -0700

    adding database to repo again

---
+++ movies.sql
@@ -0,0 +1,16 @@
+Drop Table movies;
+Drop Table wiki;
+CREATE TABLE movies (
+Title TEXT PRIMARY KEY,
+Streaming_Channel TEXT);
+select * from movies;
+
+CREATE TABLE wiki (
+film TEXT PRIMARY KEY,
+director TEXT);
+
+select * from wiki;
+
+select m.title,w.director,m.streaming_channel
+from movies m
+join wiki w on m.title=w.film;
