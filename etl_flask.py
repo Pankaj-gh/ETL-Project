5f8fbae2e7bf6fb422ea4b07242e47d42ba924ce
jddarby <67566019+jddarby1108@users.noreply.github.com>
2020-10-27 20:26:54 -0500

    adding some formatting in html

--- etl_flask.py
+++ etl_flask.py
@@ -10,27 +10,23 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
 
 from flask import Flask, jsonify
+from flask import Flask, render_template
 
 #################################################
 # Database Setup
 #################################################
-<<<<<<< HEAD:app.py
 pg_user = 'postgres'
-pg_password = 'postgres'
 db_name = 'ETL Project'
 
 connection_string = f"{pg_user}:Jennifer11@localhost:5432/{db_name}"
 engine = create_engine(f'postgresql://{connection_string}')
 
-=======
-# engine = create_engine("postgres:///movies.sql")
->>>>>>> 7b5250cb13d311563a507215e3e98e48f631b87a:etl_flask.py
 
-pg_user = 'postgres'
-db_name = 'ETL Project'
+# pg_user = 'postgres'
+# db_name = 'ETL Project'
 
-connection_string = f"{pg_user}:3963@localhost:5432/{db_name}"
-engine = create_engine(f'postgresql://{connection_string}')
+# connection_string = f"{pg_user}:3963@localhost:5432/{db_name}"
+# engine = create_engine(f'postgresql://{connection_string}')
 # reflect an existing database into a new model
 Base = automap_base()
 Base.prepare(engine, reflect=True)
@@ -61,11 +57,7 @@ def welcome():
 
 
 @app.route("/api/v1.0/wiki")
-<<<<<<< HEAD:app.py
 def wiki_():
-=======
-def wikis():
->>>>>>> 7b5250cb13d311563a507215e3e98e48f631b87a:etl_flask.py
     """Return a list of all movie names"""
 
     # Query all movies
@@ -78,30 +70,28 @@ def wikis():
     # Convert list of tuples into normal list
     all_names = list(np.ravel(results))
     print(all_names)
-    return jsonify(all_names)
+    # return jsonify(all_names)
+    return render_template("index.html", all_names=all_names) 
 
 
 @app.route("/api/v1.0/movies")
-<<<<<<< HEAD:app.py
 def movies_():
-=======
-def movie_():
->>>>>>> 7b5250cb13d311563a507215e3e98e48f631b87a:etl_flask.py
     """Return a list movies and where they are streaming"""
 
     # Query all movies
     session = Session(engine)
-    results = session.query(movies.title).all()
+    results = session.query(movies.title, movies.streaming_channel).all()
 
-    # close the session to end the communication with the database
+        # close the session to end the communication with the database
     session.close()
 
     # Convert list of tuples into normal list
-    all_names = list(np.ravel(results))
-    print(all_names)
-    return jsonify(all_names)
+    # all_names = list(np.ravel(results))
+    # print(all_names)
+    
+    # return jsonify(all_names)
+    return render_template("streaming.html", all_names=results) 
 
-
-
+  
 if __name__ == '__main__':
     app.run(debug=True)
